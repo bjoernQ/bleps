071cb98428cac91de04a5ab0e6347783d5089938
bjoernQ <bjoern.quentin@mobile-j.de>
2023-12-04 12:59:16 +0100

    Pull out `async run`

--- bleps/src/async_attribute_server.rs
+++ bleps/src/async_attribute_server.rs
@@ -34,6 +34,87 @@ impl<'a, T> AttributeServer<'a, T>
 where
     T: embedded_io_async::Read + embedded_io_async::Write,
 {
+        /// Run the GATT server until disconnect
+        pub async fn run<F, R>(&mut self, notifier: &'a mut F) -> Result<(), AttributeServerError>
+        where
+            F: FnMut() -> R,
+            R: core::future::Future<Output = NotificationData>,
+        {
+            let notification_to_send = Mutex::new(RefCell::new(None));
+            loop {
+                let notifier_future = async { notifier().await };
+                let worker_future = async {
+                    let notification: Option<NotificationData> =
+                        critical_section::with(|cs| notification_to_send.borrow_ref_mut(cs).take());
+    
+                    // check if notifications are enabled for the characteristic handle
+                    let notification = if let Some(notification) = notification {
+                        let attr = self
+                            .attributes
+                            .iter()
+                            .enumerate()
+                            .find(|(_idx, attr)| attr.handle == notification.handle);
+                        let enabled = if let Some((idx, _)) = attr {
+                            // assume the next descriptor is the "Client Characteristic Configuration" Descriptor
+                            // which is always true when using the macro
+                            if self.attributes.len() > idx + 1
+                                && self.attributes[idx + 1].uuid == Uuid::Uuid16(0x2902)
+                            {
+                                let mut cccd = [0u8; 1];
+                                let cccd_len =
+                                    self.get_characteristic_value((idx + 1) as u16, 0, &mut cccd[..]);
+                                if let Some(1) = cccd_len {
+                                    cccd[0] == 1
+                                } else {
+                                    false
+                                }
+                            } else {
+                                false
+                            }
+                        } else {
+                            false
+                        };
+                        if enabled {
+                            Some(notification)
+                        } else {
+                            None
+                        }
+                    } else {
+                        None
+                    };
+    
+                    self.do_work_with_notification(notification).await
+                };
+                pin_mut!(notifier_future);
+                pin_mut!(worker_future);
+    
+                let notification = match futures::future::select(notifier_future, worker_future).await {
+                    Either::Left((notification, _)) => Some(notification),
+                    Either::Right((value, _)) => {
+                        if value? == WorkResult::GotDisconnected {
+                            break;
+                        }
+                        None
+                    }
+                };
+    
+                if let Some(notification) = notification {
+                    critical_section::with(|cs| {
+                        notification_to_send
+                            .borrow_ref_mut(cs)
+                            .replace(notification);
+                    });
+                }
+            }
+    
+            Ok(())
+        }
+}
+
+impl<'a, T> AttributeServer<'a, T>
+where
+    T: embedded_io_async::Read + embedded_io_async::Write,
+{
     pub fn new(ble: &'a mut Ble<T>, attributes: &'a mut [Attribute<'a>]) -> AttributeServer<'a, T> {
         for (i, attr) in attributes.iter_mut().enumerate() {
             attr.handle = i as u16 + 1;
@@ -94,82 +175,6 @@ where
             )
             .await;
         Ok(EventType::Unknown)
-    }
-
-    /// Run the GATT server until disconnect
-    pub async fn run<F, R>(&mut self, notifier: &'a mut F) -> Result<(), AttributeServerError>
-    where
-        F: FnMut() -> R,
-        R: core::future::Future<Output = NotificationData>,
-    {
-        let notification_to_send = Mutex::new(RefCell::new(None));
-        loop {
-            let notifier_future = async { notifier().await };
-            let worker_future = async {
-                let notification: Option<NotificationData> =
-                    critical_section::with(|cs| notification_to_send.borrow_ref_mut(cs).take());
-
-                // check if notifications are enabled for the characteristic handle
-                let notification = if let Some(notification) = notification {
-                    let attr = self
-                        .attributes
-                        .iter()
-                        .enumerate()
-                        .find(|(_idx, attr)| attr.handle == notification.handle);
-                    let enabled = if let Some((idx, _)) = attr {
-                        // assume the next descriptor is the "Client Characteristic Configuration" Descriptor
-                        // which is always true when using the macro
-                        if self.attributes.len() > idx + 1
-                            && self.attributes[idx + 1].uuid == Uuid::Uuid16(0x2902)
-                        {
-                            let mut cccd = [0u8; 1];
-                            let cccd_len =
-                                self.get_characteristic_value((idx + 1) as u16, 0, &mut cccd[..]);
-                            if let Some(1) = cccd_len {
-                                cccd[0] == 1
-                            } else {
-                                false
-                            }
-                        } else {
-                            false
-                        }
-                    } else {
-                        false
-                    };
-                    if enabled {
-                        Some(notification)
-                    } else {
-                        None
-                    }
-                } else {
-                    None
-                };
-
-                self.do_work_with_notification(notification).await
-            };
-            pin_mut!(notifier_future);
-            pin_mut!(worker_future);
-
-            let notification = match futures::future::select(notifier_future, worker_future).await {
-                Either::Left((notification, _)) => Some(notification),
-                Either::Right((value, _)) => {
-                    if value? == WorkResult::GotDisconnected {
-                        break;
-                    }
-                    None
-                }
-            };
-
-            if let Some(notification) = notification {
-                critical_section::with(|cs| {
-                    notification_to_send
-                        .borrow_ref_mut(cs)
-                        .replace(notification);
-                });
-            }
-        }
-
-        Ok(())
     }
 
     pub async fn do_work(&mut self) -> Result<WorkResult, AttributeServerError> {
